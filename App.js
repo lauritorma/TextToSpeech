f7a0d37de9037250c7074a3cc11dfe0c9f8c164a
lauritorma <bgu171@myy.haaga-helia.fi>
2023-03-14 14:08:36 +0200

    simple text to speech program with expo speech

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet,View, TextInput, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{ height: 50, width: 300, borderColor: 'black', borderWidth: 2, margin: 100, textAlign: "center" }}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Button title="Press to hear text!" onPress={speak} />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +30,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
+
+
+
